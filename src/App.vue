e86a37da408210093cea6c613e805de0cefaf5c1
WarMachineEngine <WarMachineEngine@gmail.com>
2019-10-09 17:25:18 -0500

    Bootstrap-vueJs Added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Plantillas />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Plantillas from './components/Plantillas.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Plantillas
   }
 }
 </script>
---
+++ src/components/Plantillas.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Administración de Plantillas</h1>
+</template>
+
+<script>
+    export default {
+        name: 'Plantillas',  
+    }
+</script>
+
+<style>
+
+</style>
